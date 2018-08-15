f20c7307a1171afea6fe40c8ba866a1991ca1443
kevinheathhigh <kevin@hobbitsoft.net>
2018-08-14 22:49:57 -0700

    Updated Step 1: Create a Java library

---
+++ FinalProject/jokeProvider/src/main/java/net/hobbitsoft/jokeprovider/JokeProvider.java
@@ -0,0 +1,10 @@
+package net.hobbitsoft.jokeprovider;
+
+// Jokes provided by Cortana(tm)
+
+public class JokeProvider {
+
+    public String getJoke(){
+        return "What did the fisherman say to the magician? Pick a cod, any cod.";
+    }
+}
